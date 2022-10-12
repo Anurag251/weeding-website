ac2c37bae4c1b2ac8a4c4fb3cee4e4db7c0448dc
Anurag251 <rana.anurag12345@.com>
2022-10-12 14:34:59 +0545

    responsive design improved

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -433,3 +433,23 @@ if (filterBtn) {
     categories.classList.remove("active");
   });
 }
+
+/* Storing user's device details in a variable*/
+let details = navigator.userAgent;
+
+console.log(navigator.platform);
+
+/* Creating a regular expression
+      containing some mobile devices keywords
+      to search it in details string*/
+let regexp = /android|iphone|kindle|ipad/i;
+
+/* Using test() method to search regexp in details
+      it returns boolean value*/
+let isMobileDevice = regexp.test(details);
+
+if (isMobileDevice) {
+  mouse.style.display = "none";
+} else {
+  mouse.style.display = "block";
+}
